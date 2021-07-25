846ddf7a61e769eb9ea6283dd490deb8f185e4b1
harsh7012 <harsh7012@gmail.com>
2021-07-26 00:39:22 +0530

    application user added

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SUPERUSER_NAME=appuser
+SUPERUSER_EMAIL=appuser@tessian.com
+
 if [[ ${RUN_MIGRATIONS} == "yes" ]]
         then
         echo "[INFO] Running migrations......."
@@ -8,6 +11,9 @@ if [[ ${RUN_MIGRATIONS} == "yes" ]]
 else
         echo "[INFO] Skipping migrations........"
 fi
+
+echo "[INFO] Creating Superuser [${SUPERUSER_NAME}]....."
+poetry run python manage.py createsuperuser --username=${SUPERUSER_NAME} --email=${SUPERUSER_EMAIL} --no-input
 
 echo "[INFO] Starting application server......"
 #poetry run python manage.py runserver
