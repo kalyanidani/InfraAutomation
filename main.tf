b04e9d52cc47047c47e0ba9f31feff592544fe70
harsh7012 <harsh7012@gmail.com>
2021-07-27 23:57:27 +0530

    corrections

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ provider "aws" {
   profile = lookup(var.aws_profiles, var.deploy_env, "default")
 }
 
+
+locals {
+  common_tags = {
+      environment = var.deploy_env
+      app_stack = var.app_name
+  }
+}
 #-------------- data sources ----------------
 
 
@@ -26,6 +33,39 @@ provider "aws" {
 
 module "ecs_cluster" {
     source = "./aws-modules/ecs-cluster"
-    cluster_name = var.cluster_name
+    cluster_name = "${var.app_name}-ecs-cluster"
 }
 
+module "alb_security_group" {
+    source = "./aws-modules/security-group"
+    security_group_name = "${var.app_name}-lb-sg"
+    security_group_desc = "SG to allow internet incoming traffic at port 80 for ALB"
+    sec_vpc_id = lookup(var.vpc_id, var.deploy_env)
+    tags = locals.common_tags
+}
+
+resource "aws_security_group_rule" "alb_sg_ingress" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 80
+  protocol          = "HTTP"  
+  cidr_blocks       = ["0.0.0.0"]
+  security_group_id = module.alb_security_group.security_group_id
+}
+
+module "container_instance_security_group" {
+    source = "./aws-modules/security-group"
+    security_group_name = "${var.app_name}-ci-sg"
+    security_group_desc = "SG to allow traffic from ALB to container instances"
+    sec_vpc_id = lookup(var.vpc_id, var.deploy_env)
+    tags = locals.common_tags
+}
+
+resource "aws_security_group_rule" "ci_sg_ingress" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = "35000-65500"
+  protocol          = "TCP"  
+  source_security_group_id       = module.alb_security_group.security_group_id 
+  security_group_id = module.container_instance_security_group.security_group_id
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,20 @@
-variable "cluster_name" {
+
+
+variable "app_name" {
     type = string
-    description = "Cluster name"  
+    description = "Application short name"  
+}
+
+variable "deploy_env" {
+    type        = string
+  description = "SDLC env for deploying the ecs cluster"
+  validation {
+    condition     = contains(["dev", "qa", "stage", "prod"], var.deploy_env)
+    error_message = "The deploy_env can only assume either of the values: [dev, qa, stage, prod]."
+  }
+}
+
+aws_profiles = {
+  "dev" = "default"
+  "qa"  = "qaprofile"
 }
